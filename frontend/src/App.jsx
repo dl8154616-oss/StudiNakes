17385b80026545bd8c2886d3f083ad54354e632d
Rizky Firmansyah <rizkifirmansyah648@gmail.com>
2025-11-29 15:41:03 +0700

    Menambahkan Komentar di App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+
 import Home from './pages/Home'
 import TaskList from './pages/TaskList'
 import TaskForm from './pages/TaskForm'
+
 import './index.css'
 import './app.css'
+
+// authentication
 import LoginForm from './pages/authentication/Login/login'
 import SignupForm from './pages/authentication/SignUp/signup'
 import ForgorPassword from './pages/authentication/Login/ForgotPassword'
@@ -12,29 +16,46 @@ import PasswordReset from './pages/authentication/Login/PasswordReset'
 import SetNewPassword from './pages/authentication/Login/SetNewPassword'
 import CheckYourEmail from './pages/authentication/Login/CheckYourEmail'
 
+// course
 import CourseTabsDetails from './pages/Course/CourseTabsDetails.jsx'
+
+// dashboard user
 import UserDashboard from './pages/Dashboard/UserDashboard/LayoutUserDashboard.jsx'
+
+// user profile
 import UserProfile from './pages/Profile/LayoutUser.jsx'
 
 export default function App() {
-return (
-<div className="">
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/tasks" element={<TaskList />} />
-<Route path="/tasks/new" element={<TaskForm />} />
-<Route path="/tasks/:id/edit" element={<TaskForm />} />
-<Route path="/login" element={<LoginForm />} />
-<Route path="/signup" element={<SignupForm />} />
-<Route path="/ForgotPassword" element={<ForgorPassword />} />
-<Route path="/PasswordReset" element={<PasswordReset />} />
-<Route path="/SetNewPassword" element={<SetNewPassword />} />
-<Route path="/CheckYourEmail" element={<CheckYourEmail />} />
-<Route path="/Footer" element={<Footer/>}/>
-<Route path="/CourseTabsDetails" element={<CourseTabsDetails />} />
-<Route path='/User-Dashboard' element={<UserDashboard />} />
-<Route path='/User-Profile' element={<UserProfile />} />
-</Routes>
-</div>
-)
+  return (
+    <div className="">
+      <Routes>
+
+        {/* home */}
+        <Route path="/" element={<Home />} />
+
+        {/* tasks */}
+        <Route path="/tasks" element={<TaskList />} />
+        <Route path="/tasks/new" element={<TaskForm />} />
+        <Route path="/tasks/:id/edit" element={<TaskForm />} />
+
+        {/* authentication */}
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/ForgotPassword" element={<ForgorPassword />} />
+        <Route path="/PasswordReset" element={<PasswordReset />} />
+        <Route path="/SetNewPassword" element={<SetNewPassword />} />
+        <Route path="/CheckYourEmail" element={<CheckYourEmail />} />
+
+        {/* course details */}
+        <Route path="/CourseTabsDetails" element={<CourseTabsDetails />} />
+
+        {/* dashboard user */}
+        <Route path='/User-Dashboard' element={<UserDashboard />} />
+
+        {/* user profile */}
+        <Route path='/User-Profile' element={<UserProfile />} />
+
+      </Routes>
+    </div>
+  )
 }
